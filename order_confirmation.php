55f759d724de11032ee1d04279cd8af31276098d
Sang Le <lesan6021@gmail.com>
2019-04-08 22:01:01 -0400

    Add comment header and comment

--- order_confirmation.php
+++ order_confirmation.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Sang Le
+ * Date: 4/8/2019
+ * Time: 2:27 PM
+ * Description: Order confirmation page when the user click the submit. Will show what was order and the price.
+ * File: order_confirmation.php
+ */
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require 'flavors.php';
-
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -19,9 +28,13 @@ require 'flavors.php';
     <p>Order Summary:</p>
     <ul>
         <?php
+        // check if a flavor is set
         if(isset($_POST['flavors'])) {
+            // looping through the flavor in the form; this will return the key
             foreach ($_POST['flavors'] as $value) {
+                // looping through the flavor in the associative array
                 foreach ($flavors as $key => $item) {
+                    // check if the key in the form and key in the array match
                     if($value == $key) {
                         echo '<li>' . $item . '</li>';
                     }
