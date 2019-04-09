7b086d9433c7c6a16a68c092c80cc13c2c52b4d0
Sang Le <lesan6021@gmail.com>
2019-04-08 20:31:54 -0400

    Create associative array for cupcake flavors

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$flavors = array(
+    'grasshopper' => 'The Grasshopper',
+    'maple' => 'Whiskey Maple Bacon',
+    'carrot' => 'Carrot Walnut',
+    'caramel' => 'Salted Caramel Cupcake',
+    'velvet' => 'Red Velvet',
+    'lemon' => 'Lemon Drop',
+    'tiramisu' => 'Tiramisu'
+);
+
+$price = 3.50;
