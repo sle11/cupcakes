5591c2d9996dca845ab89441c9d86052a05dd1dd
Sang Le <lesan6021@gmail.com>
2019-04-08 22:00:15 -0400

    Add comment header

--- flavors.php
+++ flavors.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Sang Le
+ * Date: 4/8/2019
+ * Time: 2:27 PM
+ * Description: Associative array for the cupcake flavors and pricing.
+ * File: flavors.php
+ */
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
